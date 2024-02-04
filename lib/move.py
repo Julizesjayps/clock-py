8ef26723b45586a451c50094ae104759d2591f33
Julia P. Souza <julia10.jps@gmail,com>
2024-02-04 12:59:19 -0300

    deleting folders and updating the project

--- lib/move.py
+++
@@ -1,7 +0,0 @@
-from tkinter import * 
-import getpass
-
-def move_window(root, move_button):
-    
-    move_button.bind('<B3-Motion>')
-     
--- main.py
+++ main.py
@@ -3,40 +3,38 @@ from datetime import date, datetime
 import time 
 
 
-from lib import * 
-
 root = Tk()
 root.title("Digital Clocks")
 
 
 letter = ("Boulder", 150, "bold")
-
 image_file = PhotoImage(file="images_clock/clock.png")
 image = Label(root, image=image_file, bg="#fffffe")
 image.place(x=0, y=0)
 
-display = Label(root, font=letter, bg="#a1d18d", fg="#fffffe")
+display = Label(root, font=letter, bg="#a1d18d", fg="#fffffd")
 display.place(x=90, y=120)
 
-display_seconds = Label(root, font=("Boulder", 20, "bold"), bg="#a1d18d", fg="#fffffe")
+display_seconds = Label(root, font=("Boulder", 20, "bold"), bg="#a1d18d", fg="#fffffd")
 display_seconds.place(x=555, y=320)
 
-display_date = Label(root, font=("Boulder", 12, "bold"), bg="#a1d18d", fg="#fffffe")
+display_date = Label(root, font=("Boulder", 12, "bold"), bg="#a1d18d", fg="#fffffd")
 display_date.place( x=150, y=320)
 
 div_file = PhotoImage(file="images_clock/div.png")
 div = Label(root, image=div_file, bd=0)
 div.place(x=341, y=103 )
 
-close_button = Button(root, bg="#223845", activebackground="#223845", cursor="hand2", width=2, bd=0 )
-close_button.place(x= 214, y=15)
+close_button = Button(root, bg="#223845", activebackground="#084d6e", cursor="hand2", width=1, height=6, bd=0 )
+close_button.place(x= 690, y=200)
 close_button["command"] = root.destroy
 
 
-move_button = Button(root, bg="#223845", cursor="hand2", width=1, height=6 )
-move_button.place(x= 690, y = 200)
-
-move_window(root, move_button)
+def month_greeting():
+    current_month = datetime.now().strftime("%B")
+    month_label = Label(root, text="", font=("Boulder", 19, "bold"), bg="#223845", fg="#fffffd")
+    month_label.config(text=f"Happy {current_month}")  
+    month_label.place(x=250, y=15)
 
 def clocks_digital():
     tempo = time.strftime("%H %M")
@@ -52,6 +50,7 @@ def clocks_digital():
     root.after(1000, clocks_digital)  
 
 clocks_digital()
+month_greeting()
 
 
 width_window = int(root.winfo_screenwidth())
